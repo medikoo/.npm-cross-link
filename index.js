58c838b89c90b169cb21e13a4299d034b59a0984
Mariusz Nowak <medyk@medikoo.com>
2019-01-11 16:54:45 +0100

    feat: support forced refresh of package map from CLI

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 "use strict";
 
-const generatePackageNames = require("./lib/generate-packages-meta")
+const log                  = require("log").get("npm-cross-link:user").notice
+    , generatePackageNames = require("./lib/generate-packages-meta")
     , setupPrettier        = require("./lib/setup-prettier");
+
+const argv = require("minimist")(process.argv.slice(2), { boolean: ["refresh-packages-map"] });
 
 module.exports = (async () => ({
 	packagesMeta: await (async () => {
-		try {
-			return require("./lib/packages-map");
-		} catch (error) {
-			await generatePackageNames();
-			return require("./lib/packages-map");
+		if (argv["refresh-packages-map"]) {
+			log("forced refresh of packages map");
+		} else {
+			try { return require("./lib/packages-map"); }
+			catch (ignore) { log("cached packages map not found, resolving"); }
 		}
+		await generatePackageNames();
+		return require("./lib/packages-map");
 	})(),
 	hooks: { afterPackageInstall: setupPrettier },
 	userDependencies: ["prettier"]
