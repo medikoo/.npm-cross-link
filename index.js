551e8f4e1b770ee3247377abc514c5d205967c07
Mariusz Nowak <mariusz@medikoo.com>
2019-11-27 16:41:29 +0100

    feat: Autodetection of packages meta rules changes
    Additionally dropped support for `refresh-packages-map` CLI option

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 "use strict";
 
-const log                  = require("log").get("npm-cross-link:user").notice
-    , generatePackageNames = require("./lib/packages-meta/generate")
-    , setupPreCommitHook   = require("./lib/setup-pre-commit-hook")
-    , setupPrettier        = require("./lib/setup-prettier");
-
-const argv = require("minimist")(process.argv.slice(2), { boolean: ["refresh-packages-map"] });
+const setupPreCommitHook  = require("./lib/setup-pre-commit-hook")
+    , setupPrettier       = require("./lib/setup-prettier")
+    , resolvePackagesMeta = require("./lib/packages-meta");
 
 module.exports = (async () => ({
-	packagesMeta: await (async () => {
-		if (argv["refresh-packages-map"]) {
-			log("forced refresh of packages map");
-		} else {
-			try { return require("./lib/packages-meta/meta.generated"); }
-			catch (ignore) { log("cached packages map not found, resolving"); }
-		}
-		await generatePackageNames();
-		return require("./lib/packages-meta/meta.generated");
-	})(),
+	packagesMeta: await (async () => resolvePackagesMeta())(),
 	hooks: {
 		afterPackageInstall: (...args) =>
 			Promise.all([setupPrettier(...args), setupPreCommitHook(...args)])
