ee0c6e241457f474e7b8b9233b095355f712f63c
Mariusz Nowak <mariusz@medikoo.com>
2019-12-09 16:22:58 +0100

    feat: Remove now obsolete prettier setup

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Personal configuration resolver for [npm-cross-link](https://github.com/medikoo/npm-cross-link).
 
--   Resolves all my projects from github, filters ones which are not destined for npm
--   Configures hook that ensures custom version of [Prettier](https://prettier.io/) is setup for each package (so it's automatically picked by the editor)
+- Resolves all my projects from github, filters ones which are not destined for npm
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 "use strict";
 
 const setupPreCommitHook  = require("./lib/setup-pre-commit-hook")
-    , setupPrettier       = require("./lib/setup-prettier")
     , resolvePackagesMeta = require("./lib/packages-meta");
 
 module.exports = (async () => ({
 	packagesMeta: await (async () => resolvePackagesMeta())(),
-	hooks: {
-		afterPackageInstall: (...args) =>
-			Promise.all([setupPrettier(...args), setupPreCommitHook(...args)])
-	},
+	hooks: { afterPackageInstall: setupPreCommitHook },
 	userDependencies: ["prettier"],
 	toBeCopiedDependencies: ["webpack"]
 }))();
