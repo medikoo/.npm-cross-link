97af271b1f6e7b1887e8a9cb917ec5a733a32a38
Mariusz Nowak <mariusz@medikoo.com>
2020-02-27 17:57:52 +1300

    fix: Update husky hooks handling to v4

--- lib/setup-husky-hooks/husky.local.sh
+++ lib/setup-husky-hooks/husky.local.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-# husky
-
 # Created by Husky v4.2.3 (https://github.com/typicode/husky#readme)
-#   At: 2/27/2020, 5:37:32 PM
+#   At: 2/27/2020, 5:57:57 PM
 #   From: /usr/local/lib/node_modules/husky (https://github.com/typicode/husky#readme)
 
-. "$(dirname "$0")/husky.sh"
+packageManager=npm
+cd "."
