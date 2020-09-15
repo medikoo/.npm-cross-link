085c54ec9989daac0bef6db7763e62999fe47901
Mariusz Nowak <medyk@medikoo.com>
2020-09-15 12:10:52 +0200

    feat: Recognize @serverless/platform-client

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 "use strict";
 
-const setupHuskyHooks     = require("./lib/setup-husky-hooks")
+const path                = require("path")
+    , os                  = require("os")
+    , setupHuskyHooks     = require("./lib/setup-husky-hooks")
     , resolvePackagesMeta = require("./lib/packages-meta");
 
 module.exports = (async () => ({
 	packagesMeta: await (async () => resolvePackagesMeta())(),
+	multiPackageReposMeta: {
+		"serverlessinc/platform": {
+			repoUrl: "git@github.com:serverlessinc/platform.git",
+			path: path.resolve(os.homedir(), "Projects/serverless/platform")
+		}
+	},
 	hooks: { afterPackageInstall: setupHuskyHooks },
 	toBeCopiedDependencies: ["webpack"]
 }))();
