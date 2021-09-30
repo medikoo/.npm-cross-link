9a541bab2c969cf6d23eb13ca2c08d453a28c0d4
Mariusz Nowak <medyk@medikoo.com>
2021-09-30 16:22:38 +0200

    feat: Register some serverless plugins

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const path                = require("path")
 module.exports = (async () => ({
 	packagesMeta: await (async () => resolvePackagesMeta())(),
 	multiPackageReposMeta: {
+		"medikoo/serverless-chrome": {
+			repoUrl: "git@github.com:medikoo/serverless-chrome.git",
+			path: path.resolve(os.homedir(), "Projects/serverless/serverless-chrome")
+		},
 		"serverlessinc/cloud": {
 			repoUrl: "git@github.com:serverlessinc/cloud.git",
 			path: path.resolve(os.homedir(), "Projects/serverless/cloud")
